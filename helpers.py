bf9aa2b973e0e09db498106efe5e341ed60df5e1
highjhacker <mc-somango@live.fr>
2018-12-05 00:41:42 +0100

    Quickfixes

--- helpers.py
+++ helpers.py
@@ -4,7 +4,8 @@ import configparser
 
 def get_delegate_blocks_activity():
     delegate_name = get_from_config("delegate_name")
-    return requests.get(f"http://178.128.248.189:4003/api/v2/delegates/{delegate_name}").json()["data"]["blocks"]["missed"]
+    host = get_from_config("forging_node_host")
+    return requests.get(f"{host}{delegate_name}").json()["data"]["blocks"]["missed"]
 
 
 def get_from_config(key):
